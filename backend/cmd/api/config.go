5ca2cbe0c9dd7a826d0e4021a0c4dd21123f108f
raj3k <rajkosky@gmail.com>
2023-08-20 13:10:27 +0200

    db config

--- backend/cmd/api/config.go
+++ backend/cmd/api/config.go
@@ -1,13 +1,25 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func updateConfigWithVariables() (*config, error) {
 	var cfg config
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
-	flag.StringVar(&cfg.db.dsn, "db-dsn", "postgres://cfbox:pa55w0rd@localhost/cfbox?sslmode=disable", "PostgreSQL DSN")
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("CFBOX_DB_DSN"), "PostgreSQL DSN")
 
 	flag.Parse()
 
