a2461671335dbb4eadc920583d477944755e7f88
raj3k <rajkosky@gmail.com>
2023-08-27 17:58:46 +0200

    refactor: moved the Server to separate file

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -3,9 +3,6 @@ package main
 import (
 	"context"
 	"database/sql"
-	"fmt"
-	"log"
-	"net/http"
 	"os"
 	"time"
 
@@ -56,20 +53,7 @@ func main() {
 		models: data.NewModels(db),
 	}
 
-	srv := &http.Server{
-		Addr:         fmt.Sprintf(":%d", cfg.port),
-		Handler:      app.routes(),
-		ErrorLog:     log.New(logger, "", 0),
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-	}
-
-	logger.PrintInfo("starting server", map[string]string{
-		"addr": srv.Addr,
-		"env":  cfg.env,
-	})
-	err = srv.ListenAndServe()
+	err = app.serve()
 	logger.PrintFatal(err, nil)
 }
 
