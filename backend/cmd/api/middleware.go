8c58d13608027b20ee9e900ae26daecb0c9f7b13
raj3k <rajkosky@gmail.com>
2023-08-27 16:57:30 +0200

    feat: middleware for panic recovery

---
+++ backend/cmd/api/middleware.go
@@ -0,0 +1,16 @@
+package main
+
+import "net/http"
+
+func (app *application) recoverPanic(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.Header().Set("connection", "close")
+
+				app.serveErrorResponse(w, r, err.(error))
+			}
+		}()
+		next.ServeHTTP(w, r)
+	})
+}
