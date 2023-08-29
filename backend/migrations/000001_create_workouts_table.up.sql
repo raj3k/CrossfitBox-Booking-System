90a41f2f57dc683e71d107f45327c78150293a37
raj3k <rajkosky@gmail.com>
2023-08-29 20:58:04 +0200

    refactor: added NULL to time_cap, equipment and trainer_tips columns

--- backend/migrations/000001_create_workouts_table.up.sql
+++ backend/migrations/000001_create_workouts_table.up.sql
@@ -2,10 +2,10 @@ CREATE TABLE IF NOT EXISTS workouts(
     id UUID NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
     name text NOT NULL UNIQUE,
     mode text NOT NULL,
-    time_cap integer NOT NULL,
-    equipment text[],
+    time_cap integer NULL,
+    equipment text[] NULL,
     exercises text[] NOT NULL,
-    trainer_tips text[],
+    trainer_tips text[] NULL,
     created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
     updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
 );
