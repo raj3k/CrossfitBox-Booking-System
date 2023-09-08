839deb873dca5c4dab8ed51a2a2011e1b73a2447
raj3k <rajkosky@gmail.com>
2023-09-08 21:12:56 +0200

    Update SignIn.tsx with form elements.

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,6 +1,36 @@
+import {Button, Input} from "@mui/joy";
+
 const SignIn: React.FC = () => {
   return (
-    <h1>SingIn Page</h1>
+    <div className="flex flex-row justify-center items-center w-full h-full dark:bg-zinc-800">
+      <div className="w-80 max-w-full h-full py-4 flex flex-col justify-start items-center">
+        <div className="w-full py-4 grow flex flex-col justify-center items-center">
+          <form className="w-full mt-4">
+            <div className="flex flex-col justify-start items-start w-full gap-4">
+              <Input
+                className="w-full"
+                size="lg"
+                type="text"
+                placeholder="Email"
+                required
+              />
+              <Input
+                className="w-full"
+                size="lg"
+                type="password"
+                placeholder="Password"
+                required
+              />
+            </div>
+            <div className="flex flex-col justify-end items-end w-full mt-6">
+              <Button>
+                Sign Up
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
