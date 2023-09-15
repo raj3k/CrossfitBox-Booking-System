27e532b42f46378d343e92502a2ac124de46c65a
raj3k <rajkosky@gmail.com>
2023-09-15 20:55:27 +0200

    feat: extended duration of toast

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,6 +8,8 @@ import { Toaster } from 'react-hot-toast';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <Toaster position='top-center' />
+    <Toaster position='top-center' toastOptions={{
+      duration: 3000,
+    }} />
   </React.StrictMode>,
 )
