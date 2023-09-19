818d4dbe82aa2598f567b44640942bbbac9a7fe0
raj3k <rajkosky@gmail.com>
2023-09-19 20:02:45 +0200

    refactor: updated User interface for better use with axios request

--- frontend/src/types/user.d.ts
+++ frontend/src/types/user.d.ts
@@ -1,19 +1,23 @@
-interface User {
-  id: string,
-  email: string,
-  first_name: string,
-  last_name: string,
-  is_active: boolean,
-  is_staff: boolean,
-  is_superuser: boolean,
-  thumbnail: string,
-  created_at: number,
-  profile: Profile
+interface UserProfile {
+  id: string;
+  user_id: string;
+  phone_number: string | null;
+  birth_date: string | null;
 }
 
-interface Profile {
-  id: string,
-  user_id: string,
-  phone_number: number,
-  birth_date: number,
+interface User {
+  id: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  is_active: boolean;
+  is_staff: boolean;
+  is_superuser: boolean;
+  thumbnail: string | null;
+  created_at: string;
+  profile: UserProfile;
+}
+
+export interface UserData {
+  user: User;
 }
